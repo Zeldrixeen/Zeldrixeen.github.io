5755b9b9fac3da1ddd712e9e0a1b21e5da2aca02
Aarón Zúñiga Chacón <106333754+Zeldrixeen@users.noreply.github.com>
2022-06-23 02:59:49 -0600

    Add files via upload

---
+++ jasonaudios.js
@@ -0,0 +1,76 @@
+let jasonaudio1 = () => {
+    let audio1 = document.getElementById("audio1");
+    audio1.play()
+}
+let jasonaudio2 = () => {
+    let audio2 = document.getElementById("audio2");
+    audio2.play()
+}
+let jasonaudio3 = () => {
+    let audio3 = document.getElementById("audio3");
+    audio3.play()
+}
+let jasonaudio4 = () => {
+    let audio4 = document.getElementById("audio4");
+    audio4.play()
+}
+let jasonaudio5 = () => {
+    let audio5 = document.getElementById("audio5");
+    audio5.play()
+}
+let jasonaudio6 = () => {
+    let audio6 = document.getElementById("audio6");
+    audio6.play()
+}
+let jasonaudio7 = () => {
+    let audio7 = document.getElementById("audio7");
+    audio7.play()
+}
+let jasonaudio8 = () => {
+    let audio8 = document.getElementById("audio8");
+    audio8.play()
+}
+let jasonaudio9 = () => {
+    let audio9 = document.getElementById("audio9");
+    audio9.play()
+}
+let jasonaudio10 = () => {
+    let audio10 = document.getElementById("audio10");
+    audio10.play()
+}
+let jasonaudio11 = () => {
+    let audio11 = document.getElementById("audio11");
+    audio11.play()
+}
+let jasonaudio12 = () => {
+    let audio12 = document.getElementById("audio12");
+    audio12.play()
+}
+let jasonaudio13 = () => {
+    let audio13 = document.getElementById("audio13");
+    audio13.play()
+}
+let jasonaudio14 = () => {
+    let audio14 = document.getElementById("audio14");
+    audio14.play()
+}
+let jasonaudio15 = () => {
+    let audio15 = document.getElementById("audio15");
+    audio15.play();
+}
+let jasonaudio16 = () => {
+    let audio16 = document.getElementById("audio16");
+    audio16.play();
+}
+let jasonaudio17 = () => {
+    let audio17 = document.getElementById("audio17");
+    audio17.play();
+}
+let jasonaudio18 = () => {
+    let audio18 = document.getElementById("audio18");
+    audio18.play();
+}
+let jasonaudio19 = () => {
+    let audio19 = document.getElementById("audio19");
+    audio19.play();
+}
